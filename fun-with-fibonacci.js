55fdff5a4ba0665af8f7adc306190fb5ed4b1392
Abhay Jain <abhayjain13@users.noreply.github.com>
2020-12-20 13:53:46 +0530

    Fibonacci Series

---
+++ fun-with-fibonacci.js
@@ -0,0 +1,20 @@
+/*
+    Fibonacci Series Algorithm:
+    Step 1. Initialize a variable(x) that will print the output 
+    Step 2. Set a variable(next) with value 1 and intialize a temp variable to store value.
+    Step 2. Set the number till which of Fibonacci series to be printed(number).
+    Step 4. Use loop for the following steps
+        -> temp = next
+        -> next = next + x
+        -> x = temp
+        -> print the value to show
+*/
+
+const number = 50; // Set the number
+for (
+  let x = 0, next = 1, temp;
+  x < number;
+  temp = next, next = next + x, x = temp
+) {
+  console.log(x);
+}
